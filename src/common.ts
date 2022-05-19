b6a141c85a8e786676853654151cdec0d45f2d5b
TrojanerHD <xxitztrojanerhdpvptvytxx@gmail.com>
2022-05-19 21:25:33 +0200

    Change from node-fetch to https.request

--- src/common.ts
+++ src/common.ts
@@ -3,22 +3,10 @@ interface AccessToken {
   expires_at: number;
 }
 export default abstract class Common {
-  static #fetch?: typeof import('node-fetch');
   public static _discordAccessToken?: AccessToken;
 
   public static sanitize(content: string): string {
     return content.replace(/([_*\\|`])/g, '\\$1');
-  }
-
-  /**
-   * Returns the fetch module and caches it
-   */
-  public static async fetch(): Promise<typeof import('node-fetch')> {
-    if (Common.#fetch === undefined)
-      Common.#fetch = await import('node-fetch').catch((error: any) => {
-        throw new Error(`Could not import node-fetch\n${error}`);
-      });
-    return Common.#fetch;
   }
 
   public static accessTokenValid(): boolean {
