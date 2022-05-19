f5d6f6b92d64cf5c713953ae2b3262a99c6a4fa2
TrojanerHD <xxitztrojanerhdpvptvytxx@gmail.com>
2022-05-19 16:36:20 +0200

    Update node-fetch version to 3.x and discord.js

--- src/common.ts
+++ src/common.ts
@@ -1,5 +1,18 @@
 export default abstract class Common {
+  static #fetch?: typeof import('node-fetch');
+
   public static sanitize(content: string): string {
     return content.replace(/([_*\\|`])/g, '\\$1');
+  }
+
+  /**
+   * Returns the fetch module and caches it
+   */
+  public static async fetch(): Promise<typeof import('node-fetch')> {
+    if (Common.#fetch === undefined)
+      Common.#fetch = await import('node-fetch').catch((error: any) => {
+        throw new Error(`Could not import node-fetch\n${error}`);
+      });
+    return Common.#fetch;
   }
 }
