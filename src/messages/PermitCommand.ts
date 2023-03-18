e1f97482a36aa0b4a5e7590de5ae8348b24a5901
TrojanerHD <github@trojaner.dev>
2023-03-18 12:43:16 +0100

    Use role id for permit command and add list subcommand

--- src/messages/PermitCommand.ts
+++ src/messages/PermitCommand.ts
@@ -13,29 +13,42 @@ import MessageHandler from './MessageHandler';
 export default class PermitCommand extends Command {
   deploy: ChatInputApplicationCommandData = {
     name: 'permit',
-    description: 'Manages roles that are able to execute admin commands',
+    description: 'Manage roles that are able to execute admin commands',
     options: [
       {
-        type: 3,
-        name: 'option',
-        description: 'Whether to add/remove a permitted role',
-        required: true,
-        choices: [
+        type: 1,
+        name: 'manage',
+        description: 'Add/remove a permitted role',
+        options: [
           {
-            name: 'add',
-            value: 'add',
+            type: 3,
+            name: 'option',
+            description: 'Whether to add or remove',
+            required: true,
+            choices: [
+              {
+                name: 'add',
+                value: 'add',
+              },
+              {
+                name: 'remove',
+                value: 'remove',
+              },
+            ],
           },
           {
-            name: 'remove',
-            value: 'remove',
+            type: 8,
+            name: 'role',
+            description: 'The role to add/remove',
+            required: true,
           },
         ],
       },
       {
-        type: 8,
-        name: 'role',
-        description: 'The role to add/remove',
-        required: true,
+        type: 1,
+        name: 'list',
+        description: 'List all permitted roles',
+        options: [],
       },
     ],
     defaultMemberPermissions: Permissions.FLAGS.MANAGE_GUILD,
@@ -55,51 +68,74 @@ export default class PermitCommand extends Command {
     }
 
     const info: GuildInfo = await GuildSettings.settings(interaction.guildId);
-    const newRole: string = args[1].value!.toString();
-    const alreadyAdded: boolean = info.permissionRoles.some(
-      (role: string): boolean => newRole === role
-    );
-    switch (args[0].value) {
-      case 'add':
-        if (alreadyAdded) {
-          interaction
-            .reply({
-              content: `Role ${args[1].role!.name} is already a permitted role`,
-              ephemeral: true,
-            })
-            .catch(console.error);
-          return;
-        }
 
-        info.permissionRoles.push(newRole);
-        interaction
-          .reply({
-            content: `The role ${
-              args[1].role!.name
-            } has been added as a permitted role`,
-            ephemeral: true,
-          })
-          .catch(console.error);
-        break;
-      case 'remove':
-        if (!alreadyAdded) {
-          interaction
-            .reply({
-              content: `Role ${args[1].role!.name} is not a permitted role`,
-              ephemeral: true,
-            })
-            .catch(console.error);
-          return;
-        }
+    switch (args[0].name) {
+      case 'manage':
+        const options: CommandInteractionOption<CacheType>[] = args[0].options!;
+        const newRoleId: string = options[1].role!.id;
+        const newRoleIdFormatted: string = `<@&${newRoleId}>`;
 
-        info.permissionRoles = info.permissionRoles.filter(
-          (role: string) => newRole !== role
+        const alreadyAdded: boolean = info.permissionRoles.some(
+          (role: string): boolean => newRoleId === role
         );
+        switch (options[0].value) {
+          case 'add':
+            if (alreadyAdded) {
+              interaction
+                .reply({
+                  content: `Role ${newRoleIdFormatted} is already a permitted role`,
+                  ephemeral: true,
+                })
+                .catch(console.error);
+              return;
+            }
+
+            info.permissionRoles.push(newRoleId);
+            interaction
+              .reply({
+                content: `The role ${newRoleIdFormatted} has been added as a permitted role`,
+                ephemeral: true,
+              })
+              .catch(console.error);
+            break;
+          case 'remove':
+            if (!alreadyAdded) {
+              interaction
+                .reply({
+                  content: `Role ${newRoleIdFormatted} is not a permitted role`,
+                  ephemeral: true,
+                })
+                .catch(console.error);
+              return;
+            }
+
+            info.permissionRoles = info.permissionRoles.filter(
+              (role: string) => newRoleId !== role
+            );
+            interaction
+              .reply({
+                content: `Role ${newRoleIdFormatted} has been removed from the permitted roles`,
+                ephemeral: true,
+              })
+              .catch(console.error);
+            break;
+        }
+        break;
+      case 'list':
+        if (info.permissionRoles.length === 0) {
+          interaction
+            .reply({
+              content: 'Currently, no roles are permitted',
+              ephemeral: true,
+            })
+            .catch(console.error);
+          return;
+        }
         interaction
           .reply({
-            content: `Role ${
-              args[1].role!.name
-            } has been removed from the permitted roles`,
+            content: `Currently, the following roles are permitted: ${info.permissionRoles
+              .map((role: string) => `<@&${role}>`)
+              .join(', ')}`,
             ephemeral: true,
           })
           .catch(console.error);
